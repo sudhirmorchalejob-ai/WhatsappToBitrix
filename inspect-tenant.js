c986cfa803670b732901b3179e4f43e98668e69a
saurabh19000 <saurabh19003@gmail.com>
2026-08-25 17:53:10 +0530

    Change the env

--- inspect-tenant.js
+++ inspect-tenant.js
@@ -3,8 +3,10 @@ const prisma = require('./src/database/prisma');
 async function main() {
   const users = await prisma.user.findMany({ include: { tenant: true } });
   const tenants = await prisma.tenant.findMany();
+  const settings = await prisma.setting.findMany();
   console.log('USERS:', JSON.stringify(users, null, 2));
   console.log('TENANTS:', JSON.stringify(tenants, null, 2));
+  console.log('SETTINGS:', JSON.stringify(settings, null, 2));
 }
 
 main().then(() => prisma.$disconnect());
